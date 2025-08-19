df700733a601f92fdc5f865ebf9ddf4529de5950
sasank <kotasasank123@gmail.com>
2025-08-19 22:01:55 +0530

    1st commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-amber-200 font-bold align-middle flex justify-center'>jai sri ram</div>
+  )
+}
+
+export default App
